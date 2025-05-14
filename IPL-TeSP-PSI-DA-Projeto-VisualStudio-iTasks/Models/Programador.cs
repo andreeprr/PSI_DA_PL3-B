bfe22d3a6491f6720c36c124c629645753b859a6
Ricardoestg05 <2240096@my.ipleiria.pt>
2025-05-14 12:17:55 +0100

    criei classes, gestor, programador, utilizador

---
+++ IPL-TeSP-PSI-DA-Projeto-VisualStudio-iTasks/Models/Programador.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks.Models
+{
+    internal class Programador : Utilizador
+    {
+        public int idGestor { get; set; }
+        public int NivelExperiencia { get; set; }
+    }
+}
---
+++ IPL-TeSP-PSI-DA-Projeto-VisualStudio-iTasks/Models/Utilizador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iTasks.Models
+{
+    internal class Utilizador 
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        
+    }
+}
